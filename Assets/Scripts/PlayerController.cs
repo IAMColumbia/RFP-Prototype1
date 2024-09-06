2f8d86b4f896c7f9450e7f3db5e815bc9f5e975b
Rebecca Perko <rebeccaperko13@gmail.com>
2024-09-05 19:36:33 -0500

    Added Vehicle Forward with Vector

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,6 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         // Move the vehicle forward
-        transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward);
     }
 }
